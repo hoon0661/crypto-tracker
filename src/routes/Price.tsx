37214a857bff9713dd5f39eefcb59ee520d219fb
Hoon Kim <hoon0661@gmail.com>
2022-01-21 14:13:07 -0500

    fixed

--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,4 +1,12 @@
-function Price() {
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface ChartProps {
+  coinId: string;
+}
+
+function Price({ coinId }: ChartProps) {
   return <h1>Price</h1>;
 }
 
